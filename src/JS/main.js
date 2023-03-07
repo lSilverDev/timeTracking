11c777bcbe9eab8441971ab7d19fc9733464ff5b
llSilverDev <67278780+lSilverDev@users.noreply.github.com>
2023-03-07 16:21:03 -0300

    js values api

--- src/JS/main.js
+++ src/JS/main.js
@@ -2,6 +2,18 @@ const control = document.querySelectorAll("#card");
 const daily = document.getElementById("daily");
 const weekly = document.getElementById("weekly");
 const monthly = document.getElementById("monthly");
+var arrayCards = [];
+
+async function searchInfo(){
+    var data = await fetch("../../data.json");
+    var handle = await data.json();
+
+    handle.forEach(element => {
+        arrayCards.push(element);
+    });
+}
+
+searchInfo();
 
 daily.addEventListener("click", () => {
     control.forEach(element => {
@@ -22,9 +34,10 @@ monthly.addEventListener("click", () => {
 })
 
 function handle(element){
+    console.log(arrayCards);
     const elementHandledHour = element.querySelector("#hours");
-    elementHandledHour.innerHTML = 2 + "hrs";
+    elementHandledHour.innerHTML = 20 + "hrs";
 
     const elementHandledPeriod = element.querySelector("#previous");
-    elementHandledPeriod.innerHTML = "Last week - " + 2 + "hrs";
+    elementHandledPeriod.innerHTML = "Last week - " + 21 + "hrs";
 }
